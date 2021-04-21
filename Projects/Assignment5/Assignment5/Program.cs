8d03976cbaad6e45ed32b19cbbed88629b1c6993
Michael F <michaelf0926@gmail.com>
2021-04-21 16:59:21 -0400

    added notes

--- Projects/Assignment5/Assignment5/Program.cs
+++ Projects/Assignment5/Assignment5/Program.cs
@@ -8,12 +8,12 @@ namespace Assignment5
         static void Main()
         {
             Console.WriteLine("How old are you?");
-            int age = Convert.ToInt32(Console.ReadLine());
+            int age = Convert.ToInt32(Console.ReadLine()); //user enters age
             Console.WriteLine("Have you ever had a DUI?, please answer in \"true\" or \"false.\"");
-            bool dui = Convert.ToBoolean(Console.ReadLine());
+            bool dui = Convert.ToBoolean(Console.ReadLine()); //user enters if they had a dwi, using true or false.
             Console.WriteLine("How many speeding tickets do you have?");
-            int tickets = Convert.ToInt32(Console.ReadLine());
-            bool carIns = (age > 15 && dui != true && tickets < 3);
+            int tickets = Convert.ToInt32(Console.ReadLine()); //user enters number of tickets
+            bool carIns = (age > 15 && dui != true && tickets < 3); //created this to give back a boolean based on criteria.
             Console.WriteLine("Qualified for Insurance? " + carIns);
             
 
