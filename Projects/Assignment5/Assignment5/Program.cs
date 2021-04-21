788a30cbbb9999e42e533e27f8fdd98e2b1bbe2b
Michael F <michaelf0926@gmail.com>
2021-04-21 16:54:55 -0400

    made changes

---
+++ Projects/Assignment5/Assignment5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Assignment5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?, please answer in \"true\" or \"false.\"");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            bool carIns = (age > 15 && dui != true && tickets < 3);
+            Console.WriteLine("Qualified for Insurance? " + carIns);
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
