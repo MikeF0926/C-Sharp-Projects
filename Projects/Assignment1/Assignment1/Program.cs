6fa1f516263827075e3e62fd7b97247ca5d1df5c
Michael F <michaelf0926@gmail.com>
2021-04-20 21:08:42 -0400

    added assignment

---
+++ Projects/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remeber, we're \"acounting\" on you!");
+
+            Console.ReadLine();
+        }
+    }
+}
