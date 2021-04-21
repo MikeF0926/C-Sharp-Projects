c5fd8de5a2e72033481cb09e4303ac42109d05e2
Michael F <michaelf0926@gmail.com>
2021-04-21 15:58:21 -0400

    updated

--- Projects/Assignment 4/Assignment 4/Program.cs
+++ Projects/Assignment 4/Assignment 4/Program.cs
@@ -8,26 +8,38 @@ namespace Assignment_4
         static void Main()
         {
             Console.WriteLine("Anonymous Income Comparison Program");
-            Console.WriteLine("Person 1 \nHourly Rate? \n15 \nHours worked per week \n40");
-            Console.WriteLine("Person 2 \nHourly Rate? \n20 \nHours worker per week \n40");
             //salary for person 1
-            Console.WriteLine("Annual salary of Person 1:");
-            int rate1 = 15;
-            int hour1 = 40;
-            int week1 = 52;
-            int sal1 = (rate1 * hour1 * week1);
-            Console.WriteLine(sal1);
+            Console.WriteLine("Person1");             
+            Console.WriteLine("How much do you make an hour? ");
+            int rate1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many hours do you work a week? ");
+            int hours1 = Convert.ToInt32(Console.ReadLine());            
+            Console.WriteLine("Person 1 your annual pay is: ");
+            int total1 = (rate1 * hours1) * 52;
+            Console.WriteLine(total1);
+            Console.ReadLine();
             //salary for person 2
-            Console.WriteLine("Annual salary of Person 2:");
-            int rate2 = 20;
-            int hour2 = 40;
-            int week2 = 52;
-            int sal2 = (rate2 * hour2 * week2);
-            Console.WriteLine(sal2);
-            //showing whether person 1 makes more than person 2
-            Console.WriteLine("Does Person 1 make more money than Person 2?");
-            Console.WriteLine(sal1 > sal2);
-              
+            Console.WriteLine("Person2");
+            Console.WriteLine("How much do you make an hour? ");
+            int rate2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many hours do you work a week? ");
+            int hours2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Person 1 your annual pay is: ");
+            int total2 = (rate2 * hours2) * 52;
+            Console.WriteLine(total2);
+            Console.ReadLine();
+            // boolean totals
+            Console.WriteLine("Does Person 1 make more than Person 2?");
+            bool onevstwo = (total1 > total2);
+            Console.WriteLine(onevstwo);
+            Console.ReadLine();
+
+
+
+
+
+
+
 
             Console.ReadLine();
         }
