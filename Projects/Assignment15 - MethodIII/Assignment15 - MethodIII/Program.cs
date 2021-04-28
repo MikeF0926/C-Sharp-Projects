63581ba83cde1257eac1519e4b7e9558d5018588
Michael F <michaelf0926@gmail.com>
2021-04-28 16:08:31 -0400

    assignment 15

---
+++ Projects/Assignment15 - MethodIII/Assignment15 - MethodIII/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment15___MethodIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number");
+            int numOne = Convert.ToInt32(Console.ReadLine());//giving numbers
+            Console.WriteLine("Pick another number.");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+
+            MathOp firstOp = new MathOp(); //based off my other operation this will still take the and display the last number
+            firstOp.MyMethod(numOne, numTwo);
+
+            Console.ReadLine();
+        }
+    }
+}
