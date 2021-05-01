0094fc35e2b02d57961fe7607aca9555b2b6b973
MikeF0926 <michaelf0926@gmail.com>
2021-05-01 13:35:19 -0400

    21 update

--- Game twentyone/Game twentyone/Program.cs
+++ Game twentyone/Game twentyone/Program.cs
@@ -10,34 +10,17 @@ namespace Game_twentyone
     {
         static void Main(string[] args)
         {
-            Card card1 = new Card();
-            Card card2 = card1;
-            card1.Face = Face.Eight;
-            card2.Face = Face.King;
 
-            Console.WriteLine(card1.Face);
+            Deck deck = new Deck(); //calling method from different class
+            
+            deck.Shuffle(4);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
-            //Deck deck = new Deck(); //calling method from different class
-            //deck.Shuffle(4);
-
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            //Console.ReadLine();
-        }
-
-        
-
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
+        }                                
     }
 }
