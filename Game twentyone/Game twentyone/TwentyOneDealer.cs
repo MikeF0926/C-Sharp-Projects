eca297bf08bba25d43c0f47f16997fb461daae1c
Michael F <michaelf0926@gmail.com>
2021-05-02 01:42:30 -0400

    21 game updates

--- Game twentyone/Game twentyone/TwentyOneDealer.cs
+++ Game twentyone/Game twentyone/TwentyOneDealer.cs
@@ -8,7 +8,8 @@ namespace Game_twentyone
 {
     public class TwentyOneDealer : Dealer //inherit from Dealer class
     {
-        public List<Card> Hand { get; set; }
+        private List<Card> _hand = new List<Card>();
+        public List<Card> Hand { get { return _hand; } set { _hand = value; } }
         public bool Stay { get; set; }
         public bool isBusted { get; set; }
     }
